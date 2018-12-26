b066f06f90e1d99707c006acf031a76606682fdd
rhubinak <rhubinak@gmail.com>
2018-12-26 17:52:09 +0100

    Minified CSS

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+# Optimize CSS
+purifycss public/css/style.css public/index.html public/404.html public/tags/index.html public/tags/page/1/index.html public/posts/index.html public/posts/page/1/index.html public/posts/the_reason_for_this_blog/index.html public/posts/how_to_establish_a_llc_in_czech_republic/index.html public/page/1/index.html --min --info --out public/css/style.css
+
 # Add changes to git.
 git add .
 
